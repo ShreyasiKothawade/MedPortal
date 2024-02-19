d7358becd06cb31af58d08a0a85ba8c28a25f255
unknown <shreyasikothawade@gmail.com>
2024-02-19 19:09:27 +0530

    updated

---
+++ doctor_selection.js
@@ -0,0 +1,16 @@
+// const availableDoctors = ["Dr. Smith", "Dr. Johnson", "Dr. Brown"];
+
+// function selectDoctor() {
+//     console.log("Available Doctors:");
+//     availableDoctors.forEach((doctor, index) => {
+//         console.log(`${index + 1}. ${doctor}`);
+//     });
+
+//     // const selectedDoctorIndex = parseInt(prompt("Select a doctor by entering the corresponding number:")) - 1;
+
+// //     if (availableDoctors[selectedDoctorIndex]) {
+// //         console.log(`You have selected: ${availableDoctors[selectedDoctorIndex]}`);
+// //     } else {
+// //         console.log("Invalid selection. Please try again.");
+// //     }
+// // }
